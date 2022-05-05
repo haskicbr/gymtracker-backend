ea7971c2f3006084251931f720a812219e52f580
Vladislav <haskicbr@gmail.com>
2022-05-05 23:11:29 +0300

    add dogegym data interfaces

---
+++ src/ts/interfaces/index.ts
@@ -0,0 +1,47 @@
+interface WorkoutPlan {
+    id: string;
+    title: string;
+    description: string;
+    workouts: string[];
+}
+
+interface WorkoutGroup {
+    name: string;
+}
+
+interface WorkoutRepeat {
+    weight: number;
+    repeats: number;
+    restDuration: number;
+    repeatDuration: number;
+}
+
+interface Workout {
+    id: string | null;
+    title: string;
+    description: string;
+    repeats: WorkoutRepeat[];
+    isRepeated?: boolean;
+    additionalParams?: string[]
+}
+
+interface Settings {
+    weightStep: number;
+    language: string;
+}
+
+interface DogeGymState {
+    workoutPlanStarted: boolean;
+    workoutPlanActiveId?: string;
+    workoutPlans: WorkoutPlan[];
+    workouts: Workout[];
+    settings: Settings
+}
+
+export {
+    WorkoutPlan,
+    Workout,
+    WorkoutRepeat,
+    DogeGymState,
+    WorkoutGroup
+}
